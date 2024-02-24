2512a61e330b99018556300736e892f0e52d6402
zcimrn <zcimrn@ya.ru>
2024-02-24 11:47:42 +0400

    rewrite config and add packer installation script

--- init.lua
+++ init.lua
@@ -1,17 +1,24 @@
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.smarttab = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
+vim.api.nvim_create_autocmd('FileType', {
+    callback = function()
+        vim.opt.tabstop = 4
+        vim.opt.shiftwidth = 4
+        vim.opt.expandtab = true
+        vim.opt.smarttab = true
+        vim.opt.autoindent = true
+        vim.opt.smartindent = true
+
+        if vim.o.filetype == 'cpp' then
+            vim.opt.tabstop = 2
+            vim.opt.shiftwidth = 2
+            return
+        end
+    end,
+})
 
 vim.opt.number = true
-vim.opt.cursorline = true
 vim.opt.listchars = 'tab:\\u25b8 ,trail:-,nbsp:+'
 vim.opt.list = true
 vim.opt.scrolloff = 8
-
-vim.opt.mouse = ''
 
 vim.opt.incsearch = true
 vim.opt.ignorecase = true
@@ -19,31 +26,18 @@ vim.opt.smartcase = true
 vim.opt.hlsearch = true
 
 require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
-    use 'jeffkreeftmeijer/vim-dim'
-    use 'noahfrederick/vim-noctu'
-    use 'nvim-treesitter/nvim-treesitter'
-    use 'neovim/nvim-lspconfig'
+    use('wbthomason/packer.nvim')
+    use('neovim/nvim-lspconfig')
+    use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
+    use('nvim-tree/nvim-tree.lua')
+    use({ 'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim' })
+    use('nvim-lualine/lualine.nvim')
+    use('Mofiqul/vscode.nvim')
 end)
 
-vim.opt.termguicolors = false
-vim.cmd.colorscheme('dim')
-
-require('nvim-treesitter.configs').setup {
-    highlight = {
-        enable = true
-    },
-    indent = {
-        enable = false
-    },
-    incremental_selection = {
-        enable = true
-    },
-}
-
-require('lspconfig')['clangd'].setup {
-    cmd = {'clangd', '--background-index', '-j=4'}
-}
+require('lspconfig').clangd.setup({
+    cmd = {'clangd', '--background-index', '-j=16'}
+})
 
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
@@ -67,10 +61,41 @@ vim.api.nvim_create_autocmd('LspAttach', {
         end, opts)
         vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
         vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set({'n', 'v'}, '<space>ca', vim.lsp.buf.code_action, opts)
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
         vim.keymap.set('n', '<space>f', function()
-            vim.lsp.buf.format {async = true}
+            vim.lsp.buf.format { async = true }
         end, opts)
     end,
 })
+
+require('nvim-treesitter.configs').setup({
+     ensure_installed = {
+        'vim',
+        'vimdoc',
+        'lua',
+        'bash',
+        'c',
+        'cpp',
+        'go',
+        'python',
+        'javascript',
+    },
+    ignore_install = {'all'},
+    highlight = { enable = true },
+    indent = { enable = true },
+})
+
+require('nvim-tree').setup({})
+
+local telescope_builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, {})
+
+require('lualine').setup({
+    icons_enabled = true,
+})
+
+require('vscode').load()
